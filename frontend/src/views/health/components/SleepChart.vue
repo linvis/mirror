9b9c2fc0967eeb3c7dd1a95977dd2c18717bf803
shlinym <jarvisfironman@gmail.com>
2020-01-15 23:25:30 +0800

    use vue components

---
+++ frontend/src/views/health/components/SleepChart.vue
@@ -0,0 +1,111 @@
+<template>
+  <el-card class="box-card">
+    <div slot="header" class="clearfix">
+      <span>睡眠时长</span>
+    </div>
+    <el-row id="sleep_chart" />
+  </el-card>
+</template>
+
+<script>
+
+import Highcharts from 'highcharts'
+import { getSleepData } from '@/api/daily_evt'
+
+export default {
+  data() {
+    return {
+      sleep_chart: null,
+      sleep_duration_x: [],
+      sleep_duration_y: []
+    }
+  },
+  created() {
+    this.featchData()
+  },
+  methods: {
+    featchData() {
+      getSleepData().then(response => {
+        this.options = response.data
+        this.sleep_duration_x = response.data.date
+        this.sleep_duration_y = response.data.duration
+
+        this.initChart()
+      })
+    },
+    timeToString(time) {
+      var hour = parseInt(time / 60)
+      var min = parseInt(time % 60)
+      return parseInt(hour / 10).toString() + (hour % 10).toString() + ':' + parseInt(min / 10).toString() + (min % 10).toString()
+    },
+    initChart() {
+      Highcharts.chart('sleep_chart', {
+
+        title: {
+          text: 'Sleep Analysis'
+        },
+
+        // subtitle: {
+        //   text: 'Source: thesolarfoundation.com'
+        // },
+
+        yAxis: {
+          labels: {
+            formatter: function() {
+            //   return this.timeToString(this.value)
+              var hour = parseInt(this.value / 60)
+              var min = parseInt(this.value % 60)
+              return parseInt(hour / 10).toString() + (hour % 10).toString() + ':' + parseInt(min / 10).toString() + (min % 10).toString()
+            }
+          },
+          title: {
+            text: 'Hours'
+          }
+        },
+        legend: {
+          layout: 'vertical',
+          align: 'right',
+          verticalAlign: 'middle'
+        },
+
+        tooltip: {
+          formatter: function() {
+            var hour = parseInt(this.y / 60)
+            var min = parseInt(this.y % 60)
+            var time = parseInt(hour / 10).toString() + (hour % 10).toString() + ':' + parseInt(min / 10).toString() + (min % 10).toString()
+            return 'Sleep about ' + time
+          }
+        },
+
+        xAxis: {
+          categories: this.sleep_duration_x
+        //   categories: ['2019-12-31', '2020-1-1', '2020-1-2', '2020-1-3', '2020-1-4', '2020-1-5']
+        },
+
+        series: [{
+          name: 'sleep time',
+          data: this.sleep_duration_y
+        //   data: [540, 478, 464, 449, 531, 502]
+        }],
+
+        responsive: {
+          rules: [{
+            condition: {
+              maxWidth: 500
+            },
+            chartOptions: {
+              legend: {
+                layout: 'horizontal',
+                align: 'center',
+                verticalAlign: 'bottom'
+              }
+            }
+          }]
+        }
+
+      })
+    }
+
+  }
+}
+</script>
